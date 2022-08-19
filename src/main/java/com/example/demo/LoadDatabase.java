387c4b5159e9916a88164a3d2bac9481d51f79ec
Pat McKillen <pmckillen@gmail.com>
2022-08-19 19:12:49 +0100

    Added smoke on the water

--- src/main/java/com/example/demo/LoadDatabase.java
+++ src/main/java/com/example/demo/LoadDatabase.java
@@ -19,6 +19,8 @@ class LoadDatabase {
             log.info("Preloading " + repository.save(new MusicItem("Ghost Town", "The Specials", "Ska")));
             log.info("Preloading " + repository.save(new MusicItem("Let There Be Rock", "ACDC", "Hard Rock")));
             log.info("Preloading " + repository.save(new MusicItem("Stairway to Heaven", "Led Zeppelin", "Heavy Metal")));
+            log.info("Preloading " + repository.save(new MusicItem("Smoke on the water", "Deep Purple", "Heavy Metal")));
+ 
         };
     }
 }
